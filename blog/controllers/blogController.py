b06f353e86a551bd7f66f72dd28a24d5e249dbaf
Matthieu Pouille <pouillematthieu@gmail.com>
2015-03-09 18:09:47 +0000

    Update of the blog index action - add a paginator to reduce number of entries on the home page

--- blog/controllers/blogController.py
+++ blog/controllers/blogController.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
 from django.views import generic
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from blog.models.entry import Entry 
 
-class IndexAction(generic.ListView):
-	queryset = Entry.objects.published()
+class IndexAction(generic.View):
+	
+	page = None
+	paginate_by = 3
+	
 	template_name = "home.html"
-	# custom vars (used inside the view)
-	paginate_by = 5
+
+	def get(self, request, page = 1) : 
+		entry_list = Entry.objects.all()
+		paginator = Paginator(entry_list, self.paginate_by) 
+
+		if self.page == None :
+			self.page = self.kwargs['page']
+
+		try:
+			entries = paginator.page(self.page)
+		except PageNotAnInteger:
+			# If page is not an integer, deliver first page.
+			entries = paginator.page(1)
+		except EmptyPage:
+			# If page is out of range (e.g. 9999), deliver last page of results.
+			entries = paginator.page(paginator.num_pages)
+
+		return render(request, self.template_name, {'entries': entries})
+
 
 class DetailAction(generic.DetailView):
 	model = Entry
--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,9 @@ from blog.controllers import feedController, blogController
 
 urlpatterns = patterns(
 	'',
-    url(r'^feed/', feedController.IndexAction(), name="feed"),
-    url(r'^$', blogController.IndexAction.as_view(), name="home"),
+    url(r'^feed/', feedController.IndexAction, name="feed"),
+    url(r'^$', blogController.IndexAction.as_view(page=1), name="home"),
+    url(r'^page/(?P<page>\d+)$', blogController.IndexAction.as_view(), name="page"),
     url(r'^(?P<slug>\S+)$', blogController.DetailAction.as_view(), name="article_detail"),
 )
 
